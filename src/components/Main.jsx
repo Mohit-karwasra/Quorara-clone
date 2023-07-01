563a3bf3a1dc631948f35507d1fff39bca238b08
Mohit-karwasra <97739963+Mohit-karwasra@users.noreply.github.com>
2023-07-01 18:25:51 +0530

    Navbar styling

---
+++ src/components/Main.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+// import styles from "../styles/main.module.css";
+import { Navbar } from "./Navbar";
+
+export function Main() {
+	return (
+		<div id="container">
+			<Navbar />
+		</div>
+	);
+}
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import styles from "../styles/navbar.module.css";
+import logo from "../assets/kindpng_4620573.png";
+import home from "../assets/home_1946488.png";
+import followers from "../assets/play_10111766.png";
+import answer from "../assets/writing_5812594.png";
+import spaces from "../assets/people.png";
+import notification from "../assets/bell.png";
+import search from "../assets/magnifying-glass.png";
+import avatar from "../assets/man.png";
+import language from "../assets/globe.png";
+
+export function Navbar() {
+	return (
+		<div className={styles["navbar"]}>
+			<div className={styles["logo-img-container"]}>
+				<img src={logo} alt="logo" className={styles["logo-img"]} />
+			</div>
+			<div className={styles["icons-container"]}>
+				<div className={styles["icon"]}>
+					<img src={home} alt="home icon" className={styles["icon-img"]} />
+				</div>
+				<div className={styles["icon"]}>
+					<img src={followers} alt="followers icon" className={styles["icon-img"]} />
+				</div>
+				<div className={styles["icon"]}>
+					<img src={answer} alt="answer icon" className={styles["icon-img"]} />
+				</div>
+				<div className={styles["icon"]}>
+					<img src={spaces} alt="spaces icon" className={styles["icon-img"]} />
+				</div>
+				<div className={styles["icon"]}>
+					<img src={notification} alt="notification icon" className={styles["icon-img"]} />
+				</div>
+			</div>
+			<div className={styles["search-input"]}>
+				<img src={search} alt="search-icon" className={styles["search-icon"]} />
+				<input type="text" placeholder="Search Quora" className={styles["input"]} />
+			</div>
+			<div className={styles["profile-language-button-container"]}>
+				<div className={styles["avatar-holder"]}>
+					<img src={avatar} alt="avatar" className={styles["avatar-img"]} />
+				</div>
+				<img src={language} alt="language" className={styles["language-icon"]} />
+				<button className={styles["add-question-btn"]}>Add Question</button>
+			</div>
+		</div>
+	);
+}
